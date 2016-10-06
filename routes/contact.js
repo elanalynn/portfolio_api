89e81ad1e2ecfa4d7c2bf9208952d951777bdb56
Elana Kopelevich <ekopelevich@gmail.com>
2016-10-06 15:50:00 -0600

    uninstall sendgrid, try nodemailer

--- routes/contact.js
+++ routes/contact.js
@@ -1,28 +1,40 @@
 const express = require('express')
 const router = express.Router()
+const nodeMailer = require('nodemailer')
 
-const helper = require('sendgrid').mail
-const from_email = new helper.Email('elanalynnnnn@gmail.com')
-const to_email = new helper.Email('elanalynnnnn@gmail.com')
-const subject = 'Hello World from the SendGrid Node.js Library!'
-const content = new helper.Content('text/plain', 'Hello, Email!')
-const mail = new helper.Mail(from_email, subject, to_email, content)
+// const helper = require('sendgrid').mail
+// const from_email = new helper.Email('elanalynnnnn@gmail.com')
+// const to_email = new helper.Email('elanalynnnnn@gmail.com')
+// const subject = 'Message from elanalynn.com...'
+// const content = new helper.Content('text/plain', 'Hello, Email!')
+// const mail = new helper.Mail(from_email, subject, to_email, content)
+//
+// const sg = require('sendgrid')(process.env.SENDGRID_API_KEY)
 
-const sg = require('sendgrid')(process.env.SENDGRID_API_KEY)
-const request = sg.emptyRequest({
-  method: 'POST',
-  path: '/v3/mail/send',
-  body: mail.toJSON(),
-})
+router.post('/', (req, res) => {
 
-sg.API(request, (error, res) => {
-  console.log(res.statusCode)
-  console.log(res.body)
-  console.log(res.headers)
-})
+  const messageBody =
+  `<h3>From: ${req.body.name} </h3>
+  <h4>Email: ${req.body.email}</h4>
+  <p>${req.body.note}</p>`
 
-router.get('/', (req, res) => {
-  res.send('contact')
+  const mailOptions = {
+    from: 'elanalynnnnn@gmail.com',
+    to: 'elanalynnnnn@gmail.com',
+    subject: 'Message from elanalynn.com',
+    html: messageBody,
+  }
+
+  let transporter = nodeMailer.createTransport('smtps://elanalynnnnn%40gmail.com:pass@smtp.gmail.com')
+
+  transporter.sendMail(mailOptions, (err, info) => {
+    if(err){
+      console.log(err)
+      res.json({yo: 'err'})
+    }
+    console.log('Message sent: ' + info.response)
+    res.json({yo: info.response})
+  })
 })
 
 module.exports = router
